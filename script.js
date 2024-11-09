a4823cf972a7cb552939defedc121bef61a37f33
Jaime Roman <153094375+Zenkuj@users.noreply.github.com>
2024-11-09 13:54:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ function searchImages() {
     const image1Container = document.getElementById('image1Container');
     const image2Container = document.getElementById('image2Container');
     const image3Container = document.getElementById('image3Container');
+    const image4Container = document.getElementById('image4Container');
+    const image5Container = document.getElementById('image5Container');
     // Ocultar ambas imágenes por defecto
     image1Container.classList.add('hidden');
     image2Container.classList.add('hidden');
-
+    image3Container.classList.add('hidden');
+    image4Container.classList.add('hidden');
+    image5Container.classList.add('hidden');
     // Mostrar las imágenes y descripciones según el nombre ingresado
-    if (input === 'encar') {
+    if (input === 'jesus') {
         image1Container.classList.remove('hidden');
-    } else if (input === 'playas') {
+    
         image2Container.classList.remove('hidden');
-    } else if (input === 'medicina') {
+    } else if (input === 'trinidad') {
         image3Container.classList.remove('hidden');
+    } else if (input === 'encarnacion') {
+        image4Container.classList.remove('hidden');
+        image4Container.classList.remove('hidden');
+    } else if (input === 'san cosme') {
+        image5Container.classList.remove('hidden');
     } else {
         alert('No se encontraron imágenes con ese nombre.');
     }
