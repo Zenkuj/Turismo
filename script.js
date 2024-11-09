60a6e54e7490db508f5bf3908d4569f8d7b19662
Jaime Roman <153094375+Zenkuj@users.noreply.github.com>
2024-11-09 19:42:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,31 @@ function searchImages() {
         image4Container.classList.remove('hidden');
     } else if (input === 'san cosme') {
         image5Container.classList.remove('hidden');
+        addClickEvent(image5Container, 'San Cosme y San Damián (comúnmente llamado San Cosme y Damián) es una de las ciudades que formó parte de los poblados fundados por la Compañía de Jesús (los jesuitas). Su origen se remonta al año 1632 y es una de las tres misiones jesuitas que sobrevivieron en Paraguay desde la expulsión de estos en 1767. Lo que más se ha preservado es su arquitectura.');
     } else {
         alert('No se encontraron imágenes con ese nombre.');
     }
+// Agregar eventos de clic a los contenedores de imagen
+
+addClickEvent(image1Container, 'Información sobre la imagen de Jesús.');
+
+addClickEvent(image2Container, 'Información sobre otra imagen de Jesús.');
+
+addClickEvent(image3Container, 'Información sobre la Trinidad.');
+
+addClickEvent(image4Container, 'Información sobre la Encarnación.');
+
+
+
+}
+
+
+function addClickEvent(container, info) {
+
+container.onclick = function() {
+
+    alert(info);
+
+};
+
 }
